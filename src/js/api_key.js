7ddc9b335d2158a9af3fec86f028fd35424be7e3
Travellin91 <118572078+Travellin91@users.noreply.github.com>
2023-04-27 01:48:50 +0300

    +

---
+++ src/js/api_key.js
@@ -0,0 +1,3 @@
+const KEY = '35806799-4c2a0f1dc5f5db1917aa24325'
+
+export {KEY};
---
+++ src/js/fluend_scroll.js
@@ -0,0 +1,18 @@
+Fluend_scroll({
+  animationTime: 800,
+
+  stepSize: 75,
+
+  acceleretionDelta: 30,
+
+  accelerationMax: 2,
+
+  keyboardSupport: true,
+
+  arrowScroll: 50,
+
+  pulseAlgorithm: true,
+  pulseScale: 4,
+  pulseNormalize: 1,
+  touchpadSupport: true,
+});
