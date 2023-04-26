d843b798128c2fc8593261cdd033adbfda20626f
Travellin91 <118572078+Travellin91@users.noreply.github.com>
2023-04-26 23:15:17 +0000

    Deploying to gh-pages from @ Travellin91/goit-js-hw-11@e54b2347615c8cb92e6300c41746ced551955367 🚀

---
+++ index.32126b37.js
@@ -0,0 +1,2 @@
+Fluend_scroll({animationTime:800,stepSize:75,acceleretionDelta:30,accelerationMax:2,keyboardSupport:!0,arrowScroll:50,pulseAlgorithm:!0,pulseScale:4,pulseNormalize:1,touchpadSupport:!0});
+//# sourceMappingURL=index.32126b37.js.map
